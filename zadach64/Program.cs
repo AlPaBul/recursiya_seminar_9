4c6317638d0ec0c514b0ca0cfcf00976c18d1bc4
Алексей Парасненко <aleksejparasnenko@MacBook-Pro-Aleksej.local>
2023-07-15 08:31:27 +0700

    zadacha66 zdelana

---
+++ zadach64/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в 
+промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+
+Console.Clear();
+
+Console.WriteLine("введите любое число N: ");
+int numberN = int.Parse(Console.ReadLine());
+
+Console.WriteLine(PrintNumber(numberN));
+
+string PrintNumber (int start)
+{
+    int end = 1;
+    if (end == start) 
+    return end.ToString();
+    return (start + " " + PrintNumber(start - 1));
+}
---
+++ zadacha68/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+Console.Clear();
+Console.WriteLine("Введите число M(начало): ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число N(конец): ");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Cумма = {PrintSumNumbers(m, n)}");
+
+int PrintSumNumbers(int m, int n)
+{
+    if (m == n) 
+    return m;
+    return (m + PrintSumNumbers(m + 1, n));
+}
+
+
